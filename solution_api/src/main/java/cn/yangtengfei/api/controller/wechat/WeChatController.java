7d5e3f7afb8076af4bb5047df27718d238c682ca
soaryang <759620299@qq.com>
2017-07-15 03:22:48 +0800

    使用maven编译项目

--- solution_api/src/main/java/cn/yangtengfei/api/controller/wechat/WeChatController.java
+++ solution_api/src/main/java/cn/yangtengfei/api/controller/wechat/WeChatController.java
@@ -74,7 +74,7 @@ public class WeChatController {
 
         String myOpenId = request.getParameter("o3QMVwI1DrAj4y3TE9VD1I4HqOFE");
 
-        String ulr ="https://api.weixin.qq.com/cgi-bin/user/info?access_token=b1f21e4d4b15be963dd58571b040515c&openid="+myOpenId+"&lang=zh_CN";
+        String ulr ="https://api.weixin.qq.com/cgi-bin/user/info?access_token=BTmIrnrTBeaGrB2po5gH21WWJlMeB&openid="+myOpenId+"&lang=zh_CN";
         logger.info("当前用户信息："+HttpUtil.sendGet(ulr,null));
 
         // 将请求、响应的编码均设置为UTF-8（防止中文乱码）
