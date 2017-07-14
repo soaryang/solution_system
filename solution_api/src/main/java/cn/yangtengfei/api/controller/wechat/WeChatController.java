88cd163d67b68682d2ffff10623c9c4802e5719c
soaryang <759620299@qq.com>
2017-07-15 01:07:09 +0800

    使用maven编译项目

--- solution_api/src/main/java/cn/yangtengfei/api/controller/wechat/WeChatController.java
+++ solution_api/src/main/java/cn/yangtengfei/api/controller/wechat/WeChatController.java
@@ -5,12 +5,19 @@ import cn.yangtengfei.api.config.Result;
 import cn.yangtengfei.api.util.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 @RestController
-@RequestMapping(value = "/v1/api/wechChat")
+@RequestMapping(value = "/wechChat")
 public class WeChatController {
 
     private static final Logger logger = LoggerFactory.getLogger(WeChatController.class);
@@ -24,5 +31,83 @@ public class WeChatController {
         logger.info("url:"+url);
         result.setData(HttpUtil.sendGet(url,null));
         return result;
+    }
+
+    @RequestMapping(value = "/valid", method = RequestMethod.GET)
+    public String valid(HttpServletRequest request, HttpServletResponse response){
+        // 微信加密签名
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+
+        String token = request.getParameter("token");
+        if (StringUtils.isEmpty(token)) {
+            token = "BTmIrnrTBeaGrB2po5gH21WWJlMeB";
+        }
+
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (checkSignature(signature, timestamp, nonce, token)) {
+            return echostr;
+        }
+        return null;
+    }
+
+    public static boolean checkSignature(String signature, String timestamp, String nonce,String token) {
+        String[] arr = new String[] { token, timestamp, nonce };
+        // 将token、timestamp、nonce三个参数进行字典序排序
+        Arrays.sort(arr);
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            content.append(arr[i]);
+        }
+        MessageDigest md = null;
+        String tmpStr = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            // 将三个参数字符串拼接成一个字符串进行sha1加密
+            byte[] digest = md.digest(content.toString().getBytes());
+            tmpStr = byteToStr(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        content = null;
+        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
+        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param byteArray
+     * @return
+     */
+    private static String byteToStr(byte[] byteArray) {
+        String strDigest = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            strDigest += byteToHexStr(byteArray[i]);
+        }
+        return strDigest;
+    }
+
+    /**
+     * 将字节转换为十六进制字符串
+     *
+     * @param mByte
+     * @return
+     */
+    private static String byteToHexStr(byte mByte) {
+        char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        char[] tempArr = new char[2];
+        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = Digit[mByte & 0X0F];
+
+        String s = new String(tempArr);
+        return s;
     }
 }
