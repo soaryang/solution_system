daf68bad1706d87c4279ba36e1c886626a145086
soaryang <759620299@qq.com>
2017-07-16 12:36:01 +0800

    COMMIT

--- solution_api/src/main/java/cn/yangtengfei/api/service/user/ApiUserService.java
+++ solution_api/src/main/java/cn/yangtengfei/api/service/user/ApiUserService.java
@@ -66,7 +66,7 @@ public class ApiUserService {
 
     public PageResultModel<UserView> findPage(int deleteFlg,int subscribeState,int index,int pageSize){
         PageResultModel<UserView> userViewPageResultModel = new PageResultModel<UserView>();
-        Page<WechatUser> wechatUserPage = wechatUserService.findWechatUserPage(0,subscribeState,index,pageSize);
+        Page<WechatUser> wechatUserPage = wechatUserService.findWechatUserPage(subscribeState,deleteFlg,index,pageSize);
         List<WechatUser> wechatUserList = wechatUserPage.getContent();
         long count  = wechatUserPage.getTotalElements();
         List<String> ids = new ArrayList<>();
