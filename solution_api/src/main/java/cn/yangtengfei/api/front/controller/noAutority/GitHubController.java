9a9a63c27964dd6719b220dd5c81661f167034fd
soaryang <longfei19880>
2017-11-12 19:15:58 +0800

    commit

--- solution_api/src/main/java/cn/yangtengfei/api/front/controller/noAutority/GitHubController.java
+++ solution_api/src/main/java/cn/yangtengfei/api/front/controller/noAutority/GitHubController.java
@@ -1,9 +1,14 @@
 package cn.yangtengfei.api.front.controller.noAutority;
 
+import cn.yangtengfei.api.front.controller.user.GitHubUserView;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Slf4j
@@ -14,7 +19,14 @@ public class GitHubController {
     @RequestMapping(value="/github/vaild",method = RequestMethod.GET)
     public String  RegisteredByGithub(String code){
         log.info("================code:{}",code);
+
         return code;
+    }
+
+    @RequestMapping(value="/node/Http",method = RequestMethod.POST)
+    public String   nodeHttpPost(GitHubUserView gitHubUserView){
+        log.info("gitHubUserView:{}",JSON.toJSONString(gitHubUserView));
+        return JSON.toJSONString(gitHubUserView);
     }
 
 }
