e1effb9e8e247ec6045d3219d320a525ffc69455
soaryang <759620299@qq.com>
2017-12-24 00:37:56 +0800

    commit

--- solution_api/src/main/java/cn/yangtengfei/api/util/RandomCodeUtil.java
+++ solution_api/src/main/java/cn/yangtengfei/api/util/RandomCodeUtil.java
@@ -8,14 +8,15 @@ import java.util.Random;
 public class RandomCodeUtil {
 
 
-    public static final String numberLowerLetterChar = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //public static final String numberLowerLetterChar = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String numberLowerLetterChar = "0123456789";
 
     public static String getCode(){
 
         Date date = DateUtils.getCurrentDate();
         StringBuffer sb = new StringBuffer();
         Random random = new Random();
-        for(int i = 0; i < 8; i++) {
+        for(int i = 0; i < 4; i++) {
             sb.append( numberLowerLetterChar.charAt( random.nextInt( numberLowerLetterChar.length())));
         }
         String result =  sb.toString();
