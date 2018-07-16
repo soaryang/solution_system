bc0071f13db515a6a722c6a9b119b2f4229636cd
soaryang <759620299@qq.com>
2018-07-17 01:25:36 +0800

    commit

--- solution_code_create_server/src/main/java/cn/yangtengfei/api/service/FileCreateService.java
+++ solution_code_create_server/src/main/java/cn/yangtengfei/api/service/FileCreateService.java
@@ -1,14 +1,24 @@
 package cn.yangtengfei.api.service;
 
 
+import cn.yangtengfei.api.config.Config;
+import cn.yangtengfei.api.util.DirectoryUtils;
+import cn.yangtengfei.api.util.FileUtil;
 import com.alibaba.fastjson.JSON;
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import springfox.documentation.spring.web.json.Json;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +27,8 @@ import java.util.Map;
 public class FileCreateService {
 
 
-    private String jsonStr = "{\"123132\":\"123132123\"}";
-
-
-
+    @Autowired
+    private Config config;
     public void createJson(){
         Map<String,Object> map = new HashMap<>();
         map.put("root","D:");
@@ -31,42 +39,64 @@ public class FileCreateService {
     @PostConstruct
     public void createFile() throws IOException {
         String projectName = "tensflowProject";
-        String rootPath = "D:" + File.separator + projectName;
-        String mainPath = rootPath+File.separator+"src"+File.separator+"main";
-        String javaPath = mainPath+File.separator+"java";
-        String resourcesPath = mainPath+File.separator+"resources";
-        String propertiesPath = resourcesPath+File.separator+"application.properties";
+        String companyName="yangtengfei";
 
-        String basePackage =javaPath +File.separator+ ("cn.yangtengfei."+projectName ).replace(".","/");
-        
+        String basePackage = "com."+companyName;
+        String rootPath = config.getRootPath(projectName);
+        System.out.println(rootPath);
+
+        boolean isHasChild = false;
+        //创建根目录
+        DirectoryUtils.createDirectory(rootPath);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("package",basePackage);
+        map.put("projectName",projectName);
+        String parentRootPath = config.getRootPath(projectName);
+
+        String freemarkDirecotry = config.getfreemarkDirecotryh();
+        if(isHasChild){
+            //TODO:有子项目处理
+            map.put("packageType","pom");
+        }else{
+            map.put("packageType","jar");
+            //创建main目录
+            String mainPath  = config.getMainPath(parentRootPath);
+            DirectoryUtils.createDirectory(config.getJavePath(mainPath));
+            String resourcePath =config.getResourcesPath(mainPath);
+            DirectoryUtils.createDirectory(resourcePath);
+            DirectoryUtils.createDirectory(config.getTestPath(mainPath));
+
+            String bussinessPath =config.getBaseBusinessPath(mainPath,basePackage);
+            //创建controller service
+            String controllerPath = bussinessPath + File.separator + "controller";
+            String servicePath = bussinessPath + File.separator + "service";
+            DirectoryUtils.createDirectory(controllerPath,servicePath);
+            //创建properties
+            String propertiesPath=config.propertiesPath(resourcePath);
+            FileUtil.createFileByFtl(propertiesPath,freemarkDirecotry,map);
+
+        }
+        String pomPath = config.pomPath(rootPath);
+        FileUtil.createFileByFtl(pomPath,freemarkDirecotry,map);
+
+        //String rootPath = "D:" + File.separator + projectName;
+        //String mainPath = rootPath+File.separator+"src"+File.separator+"main";
+        //String javaPath = mainPath+File.separator+"java";
+        //String resourcesPath = mainPath+File.separator+"resources";
+        /*String basePackage =javaPath +File.separator+ ("cn.yangtengfei."+projectName ).replace(".","/");
         String controllerPath = basePackage+File.separator+"controller";
         String servicePath = basePackage+File.separator+"service";
+        DirectoryUtils.createDirectory(javaPath,resourcesPath,controllerPath,servicePath);
 
+
+
+
+        String propertiesPath = resourcesPath+File.separator+"application.properties";
         String pomFile = rootPath + File.separator+"pom.xml";
-        createDirectory(javaPath,resourcesPath,controllerPath,servicePath);
-
-        createFile(propertiesPath,pomFile);
+        createFile(propertiesPath);
+        createPom(rootPath);*/
     }
 
-    public void createDirectory(String ...paths){
-        for(String path:paths){
-            File file = new File(path);
-            file.mkdirs();
-        }
-    }
 
-    public void createFile(String... paths)  {
 
-        for(String path:paths){
-            File f = new File(path);
-            if(!f.exists()){
-                try {
-                    f.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
 }
