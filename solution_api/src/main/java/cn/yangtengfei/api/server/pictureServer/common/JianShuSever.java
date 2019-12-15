b190861503ce7012405f9fced777493d29cf47e4
tengfei.yang <tengfei.yang@qunar.com>
2019-12-15 17:35:06 +0800

    commit

--- solution_api/src/main/java/cn/yangtengfei/api/server/pictureServer/common/JianShuSever.java
+++ solution_api/src/main/java/cn/yangtengfei/api/server/pictureServer/common/JianShuSever.java
@@ -29,24 +29,30 @@ public class JianShuSever extends PictureService{
 	public static final String COOKIE = "read_mode=day; default_font=font2; locale=zh-CN; Hm_lvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1575463378; sajssdk_2015_cross_new_user=1; __yadk_uid=oDQcYpRbxfEQf8MOULmtp5b3dZLPp0x5; remember_user_token=W1sxNjY1MjYwOV0sIiQyYSQxMSQ5S0pmZGE5Z09zb2xVUE1FVHh1eVp1IiwiMTU3NTQ2MzQzNy41MTA3MjUzIl0%3D--712e46287787e383cd6b936959dd0990e4ee485a; _m7e_session_core=ef84a42291bc6049973577839a7bd22b; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2216ed0ef5f84789-0eeb17970a6ba9-3b65410e-1049088-16ed0ef5f8525c%22%2C%22%24device_id%22%3A%2216ed0ef5f84789-0eeb17970a6ba9-3b65410e-1049088-16ed0ef5f8525c%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.baidu.com%2Flink%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%7D%2C%22first_id%22%3A%22%22%7D; Hm_lpvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1575463523";
 
 	@Override
-	public void savePictureToServer(String path) throws Exception {
-		String fileName = FileUtils.getFileName(path);
-		PictureToken pictureToken = HttpObjectResponse.getObject(PictureToken.class, String.format(GET_PICTURE_TOKEN, fileName),"GET",COOKIE);
-		log.info("pictureToken============:{}",JSON.toJSONString(pictureToken));
-		String suffix = FileUtils.stringSuffix(path);
-		String[] fileArray1 = {"file", fileName, "image/" + suffix, path};
-		List<String[]> fileParams = new ArrayList<>();
-		fileParams.add(fileArray1);
-		Map<String, String> paramMap = new HashMap<>();
-		paramMap.put("token", pictureToken.getToken());
-		paramMap.put("key", pictureToken.getKey());
-		paramMap.put("name", "x:protocol");
-		String pictureUploadResult = savePictureToServer(paramMap, fileParams, new FileInputStream(new File(path)),COOKIE);
-		PitureUploadResponse pitureUploadResponse = JSON.parseObject(pictureUploadResult, PitureUploadResponse.class);
-		log.info("pitureUploadResponse============:{}",JSON.toJSONString(pitureUploadResponse));
+	public void savePictureToServer(String path) {
+		try {
+			log.info("==============path:{}",path);
+			String fileName = FileUtils.getFileName(path);
+			PictureToken pictureToken = HttpObjectResponse.getObject(PictureToken.class, String.format(GET_PICTURE_TOKEN, fileName),"GET",COOKIE);
+			log.info("pictureToken============:{}",JSON.toJSONString(pictureToken));
+			String suffix = FileUtils.stringSuffix(path);
+			String[] fileArray1 = {"file", fileName, "image/" + suffix, path};
+			List<String[]> fileParams = new ArrayList<>();
+			fileParams.add(fileArray1);
+			Map<String, String> paramMap = new HashMap<>();
+			paramMap.put("token", pictureToken.getToken());
+			paramMap.put("key", pictureToken.getKey());
+			paramMap.put("name", "x:protocol");
+			String pictureUploadResult = savePictureToServer(paramMap, fileParams, new FileInputStream(new File(path)),COOKIE);
+			PitureUploadResponse pitureUploadResponse = JSON.parseObject(pictureUploadResult, PitureUploadResponse.class);
+			log.info("pitureUploadResponse============:{}",JSON.toJSONString(pitureUploadResponse));
 
-		String insertUrl = pitureUploadResponse.getUrl();
-		log.info("insertUrl============:{}",insertUrl);
+			String insertUrl = pitureUploadResponse.getUrl();
+			log.info("insertUrl============:{}",insertUrl);
+		}catch (Exception e){
+			log.error("savePictureToServer error",e);
+		}
+
 	}
 
 	/*public static void main(String[] args) throws Exception {
