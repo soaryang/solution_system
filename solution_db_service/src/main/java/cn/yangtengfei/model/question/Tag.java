e34f0d0b0fe50cba8ed5fcb35ecb6b890b814496
tengfei.yang <tengfei.yang@qunar.com>
2019-12-30 23:03:23 +0800

    commit

--- solution_db_service/src/main/java/cn/yangtengfei/model/question/Tag.java
+++ solution_db_service/src/main/java/cn/yangtengfei/model/question/Tag.java
@@ -3,6 +3,7 @@ package cn.yangtengfei.model.question;
 import cn.yangtengfei.model.common.BaseModel;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by Administrator on 2017/5/28 0028.
@@ -32,4 +33,15 @@ public class Tag extends BaseModel {
     /**寄存的网络地址*/
     private String netUrl;
 
+
+    public String getNetUrl() {
+        if(StringUtils.isEmpty(netUrl)){
+            netUrl = "http://cms.51jieguo.com/images"+imagePath;
+        }
+        return netUrl;
+    }
+
+    public void setNetUrl(String netUrl) {
+        this.netUrl = netUrl;
+    }
 }
